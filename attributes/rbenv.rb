0a397fa5119188e0ed3d417085885eebfd243506
slightair <arksutite@gmail.com>
2013-12-31 16:26:16 +0900

    Add rbenv recipe

---
+++ attributes/rbenv.rb
@@ -0,0 +1,14 @@
+default["rbenv"] = {
+  "rubies" => ["2.0.0-p353", "2.1.0"],
+  "global" => "2.0.0-p353",
+  "gems" => {
+    "2.0.0-p353" => [
+      { "name" => "bundler" },
+      { "name" => "rake" }
+    ],
+    "2.1.0" => [
+      { "name" => "bundler" },
+      { "name" => "rake" }
+    ]
+  }
+}
---
+++ recipes/rbenv.rb
@@ -0,0 +1,37 @@
+brew 'rbenv'
+brew 'ruby-build'
+
+rbenv_config = node["rbenv"]
+
+rbenv_config["rubies"].each do |version|
+  execute "/usr/local/bin/rbenv install #{version}" do
+    user node["current_user"]
+    environment ({'HOME' => "/Users/#{node["current_user"]}"})
+
+    not_if { File.exist? "/Users/#{node["current_user"]}/.rbenv/versions/#{version}" }
+  end
+end
+
+execute "/usr/local/bin/rbenv global #{rbenv_config["global"]}" do
+  not_if "/usr/local/bin/rbenv global | grep #{rbenv_config["global"]}"
+end
+
+rbenv_config["gems"].each do |version, gem_hashs|
+  gem_hashs.each do |h|
+    pkg = h["name"]
+    execute "gem-install-#{pkg}-in-#{version}" do
+      user node["current_user"]
+      environment ({'HOME' => "/Users/#{node["current_user"]}", 'RBENV_VERSION' => version})
+      command "gem install #{pkg}"
+
+      not_if "RBENV_VERSION=#{version} gem list | grep '^#{pkg} ' "
+    end
+  end
+end
+
+%w(/etc/profile /etc/zshenv).each do |profile|
+  execute "add rbenv setting to #{profile}" do
+    command %{ test -e #{profile} && echo 'eval "$(/usr/local/bin/rbenv init -)"' >> #{profile} }
+    not_if "grep 'rbenv init' #{profile}"
+  end
+end
